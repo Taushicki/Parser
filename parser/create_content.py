73e1e5ce1795a122295f0fc829a2b47f448b0531
Taushicki <ipanteleev193@gmail.com>
2024-04-28 07:34:55 +0300

    fixed missing news without image

--- parser/create_content.py
+++ parser/create_content.py
@@ -1,12 +1,17 @@
 from models.news import Content
  
 
-def create(item): 
+def create(item):
+    img = item.find('.//rbc_news:image/rbc_news:url', namespaces={'rbc_news': 'https://www.rbc.ru'})
+    if img == None:
+        img = ''
+    else:
+        img = img.text
     try:
         return Content(
                 title=item.find('title').text,
                 link=item.find('link').text,
-                img=tem.find('.//rbc_news:image/rbc_news:url', namespaces={'rbc_news': 'https://www.rbc.ru'}).text,
+                img=img,
                 pub_date=item.find('pubDate').text,
                 description=item.find('description').text.strip('\n'),
                 category=item.find('category').text,
