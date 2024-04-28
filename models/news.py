1f815977269e1654895116ff492df4a8789d90f0
Taushicki <ipanteleev193@gmail.com>
2024-04-28 06:48:47 +0300

    Object Aontent: field img changed the type to Any

--- models/news.py
+++ models/news.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, Field
-
+from typing import Any
 
 class Content(BaseModel):
     title: str
     link: str
-    img: str
+    img: Any
     pub_date: str
     description: str
     category: str
--- parser/create_content.py
+++ parser/create_content.py
@@ -1,21 +1,17 @@
 from models.news import Content
  
 
-def create(item):
-    img = item.find('.//rbc_news:image/rbc_news:url', namespaces={'rbc_news': 'https://www.rbc.ru'}).text
-    if img is None:
-        img = ""
+def create(item): 
     try:
         return Content(
                 title=item.find('title').text,
                 link=item.find('link').text,
-                img=img,
+                img=tem.find('.//rbc_news:image/rbc_news:url', namespaces={'rbc_news': 'https://www.rbc.ru'}).text,
                 pub_date=item.find('pubDate').text,
                 description=item.find('description').text.strip('\n'),
                 category=item.find('category').text,
                 text=item.find('.//rbc_news:full-text', namespaces={'rbc_news': 'https://www.rbc.ru'}).text.strip('\n')
                 ).dict()
     except:
-        print('Some args is missing')
         return None
                          
